a8d20d721b46e4744bc8984d7ec907e6a689d379
RashelAlvarez <rashelalvarez21@gmail.com>
2024-02-17 19:39:58 -0400

    configuration cors and fix error Acces-Control-Allow-Origin

--- src/main/java/com/apibackend/app/auth/ResourceServerConfig.java
+++ src/main/java/com/apibackend/app/auth/ResourceServerConfig.java
@@ -1,10 +1,19 @@
 package com.apibackend.app.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -14,10 +23,37 @@ public class ResourceServerConfig  extends ResourceServerConfigurerAdapter{
 	public void configure(HttpSecurity http) throws Exception {
         // TODO Auto-generated method stub
 		//aca se agrega seguridad a las rutas
-        http.authorizeRequests(requests -> requests.antMatchers(HttpMethod.GET, "/clientes", "/uploads/img/**").permitAll()
-        	/*	.antMatchers(HttpMethod.GET, "/clientes/{id}").hasAnyRole("ADMIN","USER")
-        		.antMatchers(HttpMethod.POST, "/clientes").hasRole("ADMIN")*/
-                .anyRequest().authenticated());
+        http.authorizeRequests(requests -> {
+			try {
+				requests.antMatchers(HttpMethod.GET, "/clientes", "/uploads/img/**", "/images/**").permitAll()
+				        .anyRequest().authenticated().and().cors().configurationSource(corsConfigurationSource());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
 	}
+	
+	//metodo para eliminar el error  Acces-Control-Allow-Origin header is present on the requested resource
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config= new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowCredentials(true);
+		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+		
+		UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+	
+	@Bean 
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+	
 
 }
